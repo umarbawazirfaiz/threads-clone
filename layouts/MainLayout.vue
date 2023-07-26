17327fa6e40e72164d42b63d8052db219bb867c1
Umar Bawazir <umarbawazirfaiz@gmail.com>
2023-07-26 19:30:04 +0700

    feat: main layout

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="MainLayout" class="h-screen">
+        <div class="w-full max-[500px] mx-auto">
+            <div id="TopMenu" class="w-full flex items-center justify-center px-2 h-10 my-2">
+                <img class="w-[35px]" src="/threads-logo.png" />
+            </div>
+        </div>
+
+        <div class="flex w-full max-w-[500px] mx-auto h-screen">
+            <slot />
+        </div>
+
+        <div id="BottomNav" class="fixed z-50 bottom-0 flex w-full h-[70px] border-t border-t-gray-700 bg-black">
+            <div class="flex w-full max-w-[500px] mx-auto items-center justify-around">
+                <button @click="navigateTo('/')" class="w-full h-full">
+                    <Icon name="material-symbols:home-outline" size="35" color="#fff" />
+                </button>
+                <button @click="userStore.isMenuOverlay = true" class="w-full h-full">
+                    <Icon class="mb-1" name="material-symbols:edit-square-outline-round" size="32" color="#fff" />
+                </button>
+                <button @click="userStore.isLogoutOverlay = true" class="w-full h-full">
+                    <Icon class="mb-1" name="ph:sign-out" size="32" color="#fff" />
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore()
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <MainLayout>
+        Index
+    </MainLayout>
+</template>
+
+<script setup>
+    import MainLayout from '~/layouts/MainLayout.vue'
+</script>
