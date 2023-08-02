2d790ce09280b9c033c9ab4034a86ef4db09be39
Umar Bawazir <umarbawazirfaiz@gmail.com>
2023-08-02 23:25:22 +0700

    feat: setup prisma and add API

---
+++ server/api/get-all-posts.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  let posts = await prisma.posts.findMany({
+    orderBy: { id: "desc" },
+    include: { likes: true },
+  });
+
+  return posts
+});
